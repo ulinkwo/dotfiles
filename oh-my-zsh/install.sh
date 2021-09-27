511c80cca6caf63cbba17f402f9cba313531e003
Liu Zhiping <ulinkwo@gmail.com>
2021-09-27 10:28:19 +0800

    customize my mac

---
+++ oh-my-zsh/install.sh
@@ -0,0 +1,11 @@
+git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-apple-touchbar $ZSH_CUSTOM/plugins/zsh-apple-touchbar
+
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+git clone https://github.com/wbingli/zsh-wakatime.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-wakatime
--- powerlevel10k/install.sh
+++
@@ -1,1 +0,0 @@
-git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
